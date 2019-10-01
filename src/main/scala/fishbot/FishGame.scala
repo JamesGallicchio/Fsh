c5433281cf820be8a19974e7695684a6d033a432
James <jamesgallicchio>
2019-09-30 23:20:51 -0400

    Finish fish game logic

--- src/main/scala/fishbot/FishGame.scala
+++ src/main/scala/fishbot/FishGame.scala
@@ -1,37 +1,127 @@
 package fishbot
 
-import fishbot.Card
-import fishbot.FishGame.{Player, Team}
+import scala.util.Random
 
-import scala.util.{Failure, Success, Try}
+case class Player(name: String)
+case class Team(players: Seq[Player], points: Set[HalfSuit])
+// players should be sorted with most recent player as the head
 
 object FishGame {
-  type Player = String
+  def createRandom(team1players: Seq[Player], team2players: Seq[Player]): FishGame = {
+    val deck = Random.shuffle(Card.values)
 
-  type Team = Set[Player]
-}
-case class FishGame(team1: Team, team2: Team, playerHolding: Map[Card, Player], currPlayer: Player) {
-  def request(card: Card, from: Player): Try[FishGame] = {
-    // Check if caller already has the card
-    if (playerHolding(card) == currPlayer)
-      Failure(new IllegalArgumentException("Player cannot request card in their hand."))
-    // Check if caller has another card in half-suit
-    else if (!playerHolding.exists { case (k, v) => k.halfSuit == card.halfSuit && v == currPlayer })
-      Failure(new IllegalArgumentException("Player cannot request card unless they have another card in the half-suit."))
-    else
-      Success({
-        // If called player has the card, move to caller and give another turn
-        if (playerHolding(card) == from)
-          FishGame(playerHolding updated(card, currPlayer), currPlayer)
-        // Else no cards are exchanged,
-        else
-          FishGame(playerHolding, from)
-      })
+    val jumbledPlayers = Random.shuffle(team1players ++ team2players)
+
+    val hands = deck.zip(
+      Iterator.continually(jumbledPlayers).flatten
+    ).toMap
+
+    val team1 = Team(Random.shuffle(team1players), Set.empty)
+    val team2 = Team(Random.shuffle(team2players), Set.empty)
+
+    FishGame(team1, team2, hands)
   }
+}
+case class FishGame(currTeam: Team, oppTeam: Team, hands: Map[Card, Player]) {
+  import fishbot.FishException._
+
+  private def playerHasHalfsuit(player: Player, halfSuit: HalfSuit): Boolean =
+    hands.exists { case (c, p) => c.halfSuit == halfSuit && p == player }
+
+  private def onCurrTeam(player: Player): Boolean = currTeam.players.contains(player)
+  private def currPlayer: Player = currTeam.players.head
+
+  def requestCard(target: Player, card: Card): Either[InvalidRequest, FishGame] =
+    if (onCurrTeam(target))
+      Left(TargetOnTeam)
+    else if (!playerHasHalfsuit(currPlayer, card.halfSuit))
+      Left(NoCardInHalfsuit)
+    else hands.get(card) match {
+      case None =>
+        Left(CardOutOfPlay)
+      case Some(p) if p == currPlayer =>
+        Left(AlreadyHasCard)
+      case Some(p) =>
+        Right(
+          if (p == target)
+            FishGame(
+              currTeam, oppTeam, hands.updated(card, currPlayer)
+            )
+          else
+            FishGame(
+              Team(target +: oppTeam.players.filter(_ != target), oppTeam.points),
+              currTeam,
+              hands
+            )
+        )
+    }
+
+  private def winHalfSuit(halfSuit: HalfSuit): FishGame = {
+    val newHand = hands.filter(_._1.halfSuit != halfSuit)
+
+    FishGame(
+      Team(
+        currTeam.players.filter(p => newHand.values.exists(_ == p)),
+        currTeam.points + halfSuit),
+      Team(
+        oppTeam.players.filter(p => newHand.values.exists(_ == p)),
+        oppTeam.points),
+      newHand
+    )
+  }
+
+  private def loseHalfSuit(halfSuit: HalfSuit): FishGame = {
+    val newHand = hands.filter(_._1.halfSuit != halfSuit)
+
+    FishGame(
+      Team(
+        currTeam.players.filter(p => newHand.values.exists(_ == p)),
+        currTeam.points),
+      Team(
+        oppTeam.players.filter(p => newHand.values.exists(_ == p)),
+        oppTeam.points + halfSuit),
+      newHand
+    )
+  }
+
+  def callHalfsuit(halfSuit: HalfSuit, locations: Map[Card, Player]): Either[(InvalidCall, FishGame), FishGame] =
+    if (locations.keySet != Card.halfSuitSet(halfSuit))
+      Left((MissingCard, loseHalfSuit(halfSuit)))
+    else {
+      val diff = locations.filter(_._1.halfSuit == halfSuit).toSet -- locations
+
+      if (diff.nonEmpty)
+        Left((WrongCall(diff.toMap), loseHalfSuit(halfSuit)))
+      else
+        Right(winHalfSuit(halfSuit))
+    }
+}
+
+sealed trait FishException extends Exception
+object FishException {
+  case object GameFinished    extends FishException
+  case object NoPlayers       extends FishException
+
+  sealed trait InvalidRequest extends FishException
+  sealed trait InvalidCall    extends FishException
+
+  case object NoCardInHalfsuit extends InvalidRequest with InvalidCall
+
+  case object AlreadyHasCard extends InvalidRequest
+  case object CardOutOfPlay  extends InvalidRequest
+  case object TargetOnTeam   extends InvalidRequest
+
+  case object MissingCard extends InvalidCall
+  case class WrongCall(incorrect: Map[Card, Player]) extends InvalidCall
 }
 
 sealed trait HalfSuit
 object HalfSuit {
+  val values: Set[HalfSuit] = Set(
+    LowSpades, HighSpades, LowHearts, HighHearts,
+    LowClubs, HighClubs, LowDiamonds, HighDiamonds, EightsJokers
+  )
+
   case object LowSpades    extends HalfSuit
   case object HighSpades   extends HalfSuit
   case object LowHearts    extends HalfSuit
@@ -43,9 +133,19 @@ object HalfSuit {
   case object EightsJokers extends HalfSuit
 }
 
-sealed class Card(val halfSuit: HalfSuit)
+sealed class Card (val halfSuit: HalfSuit)
 object Card {
   import HalfSuit._
+
+  def values: Set[Card] = Set(
+    S2, S3, S4, S5, S6, S7, S8, S9, SX, SJ, SQ, SK, SA,
+    H2, H3, H4, H5, H6, H7, H8, H9, HX, HJ, HQ, HK, HA,
+    C2, C3, C4, C5, C6, C7, C8, C9, CX, CJ, CQ, CK, CA,
+    D2, D3, D4, D5, D6, D7, D8, D9, DX, DJ, DQ, DK, DA,
+    RedJoker, BlackJoker
+  )
+
+  def halfSuitSet(halfSuit: HalfSuit): Set[Card] = values.filter(_.halfSuit == halfSuit)
 
   case object S2 extends Card(LowSpades)
   case object S3 extends Card(LowSpades)
@@ -105,11 +205,4 @@ object Card {
 
   case object RedJoker extends Card(EightsJokers)
   case object BlackJoker extends Card(EightsJokers)
-}
-
-object FishBot {
-
-  case class CardInfo(known: Seq[Card], possible: Seq[Card])
-  case class GameData(exposedData: CardInfo, teammates: Map[Player, CardInfo], opponents: Map[Player, CardInfo])
-
 }
